bd7a0b85428f6dd623a52c660e2f2e29d9ce6981
Alan González Quintero <156737437+AlanGlzQ@users.noreply.github.com>
2025-12-24 17:55:25 -0600

    Update main.c

--- j11/main.c
+++ j11/main.c
@@ -1,20 +1,39 @@
-//NueveDos
+//Intercambio de valores de 3 variables solicitando valores iniciales al usuario
 #include <stdio.h>
-#include <math.h>
-#define PI 3.1416
+#include <stdlib.h>
 
-int main()
-{
-    //Declaración de Variables
-    float areaCirculo, radio;
-    
-    //Solicitud al usuario y presentación del programa
-    printf("Este programa calcula el area de cualquier circulo en base a su radio \n");
-    printf("Ingresa el radio: ");
-    scanf("%f", &radio);
-    
-    //Operación 
-    areaCirculo = PI * pow(radio, 2);
+int main(){
+    //Variables
+    int a,b,c,aux;
+
+    //Solicitud al usuario
+    printf("Ingrese 3 valores enteros: \n");
+    printf("Valor a: ");
+    scanf("%d", &a);
+    printf("\nValor b: ");
+    scanf("%d", &b);
+    printf("\nValor c: ");
+    scanf("%d", &c);
+
+    //Valores originales
+    printf("\nValores Originales: \n");
+    printf("a = %d \n", a);
+    printf("b = %d \n", b);
+    printf("c = %d \n", c);
+
+    //Cambio de varoles
+    aux= a;
+    a=b;
+    b=c;
+    c=aux;
+
+    printf("\nLos nuevos valores son: ");
+
+    printf("\na = %d\n", a);
+    printf("b = %d\n", b);
+    printf("c = %d\n", c);
 
     return 0;
+
+
 }
